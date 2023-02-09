809364f393aaeda7764d912b427d2e6089619ef4
Sydney Atkinson <sydney.atkinson@gmail.com>
2023-02-09 12:04:36 -0500

    adding files to github

---
+++ script.js
@@ -0,0 +1,20 @@
+// ES5 Global Constants
+
+var PI = 3.14; 
+PI = 42; //stop me from doing this 
+
+const PI = 3.14; 
+
+//What is the difference between var and let? 
+//var you can redeclare let you can not 
+
+//What is the difference between let and const? 
+//let you can reassign where const you can not
+
+//What is the difference between let and const? 
+//let you can reassign how ever you can not redeclare; const can not be reassigned or redeclared
+
+//What is hoisting?
+//Hoisting declares all the var's first before the entire code base is loaded 
+
+//if var PI = 3.14 is declared later in the code, PI will return undefined before it's decalred instead of giving a reference error 
